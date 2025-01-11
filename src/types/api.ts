b5484fea299afec26147b6a2bbd19b73e769d59b
Philipp Grigoryev <philipp.grigoryev@okta.com>
2025-01-11 16:18:32 -0600

    fixed APIs endpoints

--- src/types/api.ts
+++ src/types/api.ts
@@ -2,28 +2,29 @@
 
 
 
-const BASE_URL = 'http://localhost:4000';
+const BASE_URL = '/api';
 
 export const API_ENDPOINTS = {
-    CAMPAIGNS: `${BASE_URL}/campaigns`,
+    CAMPAIGNS: `${BASE_URL}/report`,
+    CAMPAIGN_PREPARE: (campaignName, connectionId) => `${BASE_URL}/report/${campaignName}/connection/${connectionId}/prepare`,
     DOWNLOAD_CAMPAIGNS:  `${BASE_URL}/campaigns`,
     CAMPAIGN_CONNECTIONS: `${BASE_URL}/campaignConnections`,
-    CAMPAIGN_CONTROLS: `${BASE_URL}/campaignControls`,
+    CAMPAIGN_CONTROLS: (campaignName) => `${BASE_URL}/report/${campaignName}/controls`,
 
 
     //Users
-    USERS: `${BASE_URL}/usersdata`,
+    USERS: `${BASE_URL}/user`,
 
     //Connections
-    CONNECTIONS : `${BASE_URL}/connections`,
+    CONNECTIONS : `${BASE_URL}/connection`,
     TEST_CONNECTION:`${BASE_URL}/connections/test-connection`,
 
      //Controls
-     CONTROLS : `${BASE_URL}/controls`,
+     CONTROLS : `${BASE_URL}/control`,
      //Entitlements
-     ENTITLEMENTS : `${BASE_URL}/entitlements`,
-     ENTITLEMENTS_ACCESSLIST: `${BASE_URL}/accessSet`
-
+     ENTITLEMENTS : `${BASE_URL}/entitlement`,
+     ENTITLEMENTS_ACCESSLIST: `${BASE_URL}/accessSet`,
+     ACCESSLIST: `${BASE_URL}/access`
 
     // Add more API endpoints as needed
 };
