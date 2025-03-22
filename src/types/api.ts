10b36e5ab04ef3b5a9bf054498c0017031236c9e
Philipp Grigoryev <philipp.grigoryev@okta.com>
2025-03-22 18:10:14 -0500

    changed based API url

--- src/types/api.ts
+++ src/types/api.ts
@@ -2,7 +2,8 @@
 
 
 
-const BASE_URL = 'http://localhost:8080/api';
+// const BASE_URL = 'http://localhost:8080/api';
+const BASE_URL = '/api';
 
 export const API_ENDPOINTS = {
     CAMPAIGNS: `${BASE_URL}/report`,
